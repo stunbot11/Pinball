5bf4be4b0705f0a460fef7624ca489273ee7cc7d
stunbot11 <143148371+stunbot11@users.noreply.github.com>
2025-10-05 15:16:28 -0600

    got map to/from lines and deletes no from

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -1,7 +1,76 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public List<GameObject> mapNodes; 
+    public MoveCam cam;
+    public List<List<GameObject>> mapNodes = new List<List<GameObject>>();
+    public List<GameObject> tempNode;
+    public List<MapNode> allNodes;
+    public GameObject blankMapNode;
+    public GameObject nodeHost;
+    public GameObject startNode;
+    public GameObject endNode;
+    public float disBetFloor = 2.5f;
+    public float maxNodePosFromZero = 5;
+    public int minL = 5;
+    public int maxL = 10;
+    public int minRoomsPerFloor = 2;
+    public int maxRoomsPerFloor = 5;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Move Cam").GetComponent<MoveCam>();
+        generateMap();
+    }
+
+    public void generateMap()
+    {
+        int length = Random.Range(minL, maxL);
+        cam.yMax = (length * disBetFloor) - (disBetFloor * 1.75f);
+        endNode.transform.position = new Vector3(endNode.transform.position.x, startNode.transform.position.y + (length * disBetFloor) + disBetFloor, startNode.transform.position.z);
+        for (int i = 0; i < length; i++)
+        {
+            int width = Random.Range(minRoomsPerFloor, maxRoomsPerFloor);
+            for (int j = 0; j <= width; j++)
+            {
+                
+                GameObject newMapNode = Instantiate(blankMapNode, nodeHost.transform);
+                newMapNode.transform.position = new Vector2((float)Mathf.Lerp(-maxNodePosFromZero, maxNodePosFromZero, (float)j / (float)width), endNode.transform.position.y - (disBetFloor * i) - disBetFloor);
+                newMapNode.GetComponent<MapNode>().floor = length - i;
+                newMapNode.GetComponent<MapNode>().mapGen = this;
+                if (i > 0)
+                    newMapNode.GetComponent<MapNode>().possibleToNodes = mapNodes[i - 1];
+                else
+                    newMapNode.GetComponent<MapNode>().toNodes.Add(endNode);
+                tempNode.Add(newMapNode);
+                allNodes.Add(newMapNode.GetComponent<MapNode>());
+            }
+            List<GameObject> lel = new List<GameObject>();
+
+            for (int j = 0; j < tempNode.Count; j++)
+            {
+                lel.Add(tempNode[j]);
+                if (i == length - 1)
+                {
+                    startNode.GetComponent<MapNode>().possibleToNodes.Add(tempNode[j]);
+                    startNode.GetComponent<MapNode>().Start();
+                }
+            }
+            mapNodes.Add(lel);
+            tempNode.Clear();
+        }
+        StartCoroutine(deletasuarisrex());
+    }
+
+    IEnumerator deletasuarisrex()
+    {
+        yield return new WaitForSeconds(.1f);
+        for (int i = allNodes.Count - 1; i > 0; i--)
+        {
+            if (allNodes[i].fromNodes.Count == 0)
+                Destroy(allNodes[i].gameObject);
+        }
+    }
 }
--- Assets/Scripts/Map/MapNode.cs
+++ Assets/Scripts/Map/MapNode.cs
@@ -1,23 +1,50 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Unity.VisualScripting;
 
 public class MapNode : MonoBehaviour
 {
+    public MapGenerator mapGen;
     private LineRenderer path;
-    public List<GameObject> toNodes;
-    public List<GameObject> fromNodes;
+    public List<GameObject> toNodes = new();
+    public List<GameObject> fromNodes = new();
+    public List<GameObject> possibleToNodes;
+    public List<GameObject> test = new();
+    public bool lastFloor;
+    public int floor;
 
     public void Start()
     {
-        path = GetComponent<LineRenderer>();
-        path.positionCount = toNodes.Count * 2;
-
-        for (int i = 0; i < toNodes.Count; i++)
+        if (!lastFloor)
         {
-            path.SetPosition(i * 2, transform.position);
-            path.SetPosition((i * 2) + 1, toNodes[i].transform.position);
-            toNodes[i].GetComponent<MapNode>().fromNodes.Add(this.gameObject);
-            toNodes[i].gameObject.SetActive(true);
+            path = GetComponent<LineRenderer>();
+
+            if (floor < mapGen.mapNodes.Count)
+            {
+                for (int i = 0; i < possibleToNodes.Count; i++)
+                {
+                    test.Add(possibleToNodes[i]);
+                }
+
+                int ranNum = Random.Range(1, test.Count);
+                print(ranNum);
+                for (int i = 0; i < ranNum; i++)
+                {
+                    int nodeToAdd = Random.Range(0, test.Count);
+                    toNodes.Add(test[nodeToAdd]);
+                    test.RemoveAt(nodeToAdd);
+                }
+            }
+
+            path.positionCount = toNodes.Count * 2;
+            for (int i = 0; i < toNodes.Count; i++)
+            {
+                path.SetPosition(i * 2, transform.position);
+                path.SetPosition((i * 2) + 1, toNodes[i].transform.position);
+                toNodes[i].GetComponent<MapNode>().fromNodes.Add(this.gameObject);
+                toNodes[i].gameObject.SetActive(true);
+            }
         }
     }
 }
